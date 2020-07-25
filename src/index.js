39e9e44541c884b4bb2f5cd4129d5720cb9f776b
akumar1 <akumar1@empoweredbenefits.com>
2020-07-25 15:55:07 +0530

    Base URL changed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import axios from 'axios';
-import ContextProvider from './app/context/context';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import axios from "axios";
+import ContextProvider from "./app/context/context";
 
-axios.defaults.baseURL = "http://localhost:3004";
+axios.defaults.baseURL = "https://airline-jsonserver.herokuapp.com";
 
-ReactDOM.render(<ContextProvider><App /></ContextProvider>, document.getElementById('root'));
+ReactDOM.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
+  document.getElementById("root")
+);
